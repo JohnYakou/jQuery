fa30cbc85d2c265cbfba6cc8713b0c7dd26ea041
YakouJohn <jyakou@gmx.fr>
2022-01-06 16:31:59 +0100

    06/01/2022

---
+++ 06_exo_carousel/exo_carousel.js
@@ -0,0 +1,6 @@
+var slides = document.getElementsByClassName("chauss");
+console.log(slides);
+
+// Je récupère les info des class (noms)
+// fonction function
+// YT : Tutoriel : Comment faire un SlideShow en JavaScript
